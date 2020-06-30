9c224b0cebda31a55ca96a10974365dcc92c54bf
HSIAO-PING (PING) NI <61397022+ping830616@users.noreply.github.com>
2020-06-30 13:54:15 +0800

    Add files via upload

---
+++ BullsEyes/BullsEyes/mainform.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class mainform
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnPaint = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(675, 452);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnPaint
+            // 
+            this.btnPaint.Location = new System.Drawing.Point(12, 470);
+            this.btnPaint.Name = "btnPaint";
+            this.btnPaint.Size = new System.Drawing.Size(675, 37);
+            this.btnPaint.TabIndex = 1;
+            this.btnPaint.Text = "Paint";
+            this.btnPaint.UseVisualStyleBackColor = true;
+            this.btnPaint.Click += new System.EventHandler(this.btnPaint_Click);
+            // 
+            // mainform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 519);
+            this.Controls.Add(this.btnPaint);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "mainform";
+            this.Text = "Bitmap Graphics";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnPaint;
+    }
+}
+
---
+++ BullsEyes/BullsEyes/mainform.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class mainform : Form
+    {
+        public mainform()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPaint_Click(object sender, EventArgs e)
+        {
+            Bitmap drawingSurface = new Bitmap(1000, 1000);
+            Graphics GFX = Graphics.FromImage(drawingSurface);
+
+            GFX.FillEllipse(Brushes.Red, 0, 0, 1000, 1000);
+            GFX.FillEllipse(Brushes.White, 100, 100, 800, 800);
+            GFX.FillEllipse(Brushes.Red, 200, 200, 600, 600);
+            GFX.FillEllipse(Brushes.White, 300, 300, 400, 400);
+            GFX.FillEllipse(Brushes.Red, 400, 400, 200, 200);
+
+            pictureBox1.Image = drawingSurface;
+        }
+    }
+}
