8f4e60d6612b07129054f10ee00e88150522bc08
HSIAO-PING (PING) NI <61397022+ping830616@users.noreply.github.com>
2020-07-15 10:37:02 +0800

    TCP

---
+++ tcp/tcp/ClientForm.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace tcp
+{
+    partial class ClientForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(102, 122);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(192, 25);
+            this.txtIP.TabIndex = 0;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Font = new System.Drawing.Font("新細明體", 15F);
+            this.btnConnect.Location = new System.Drawing.Point(325, 135);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(222, 49);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect to Server";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("新細明體", 15F);
+            this.btnSend.Location = new System.Drawing.Point(441, 417);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(106, 49);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("新細明體", 15F);
+            this.lblStatus.Location = new System.Drawing.Point(207, 77);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(140, 25);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Disconnected";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(102, 173);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(192, 25);
+            this.txtPort.TabIndex = 4;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(36, 221);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(499, 169);
+            this.txtLog.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 15F);
+            this.label1.Location = new System.Drawing.Point(54, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 15F);
+            this.label2.Location = new System.Drawing.Point(43, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Port:";
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(212, 429);
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(205, 25);
+            this.txtMsg.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 15F);
+            this.label3.Location = new System.Drawing.Point(31, 424);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 25);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Send to Server:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe Marker", 20F);
+            this.label4.Location = new System.Drawing.Point(234, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 40);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Client";
+            // 
+            // ClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 495);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtIP);
+            this.Name = "ClientForm";
+            this.Text = "Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ tcp/tcp/ClientForm.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+namespace tcp
+{
+    public partial class ClientForm : Form
+    {
+        Socket client;
+        byte[] buffer = new byte[1024];
+
+
+        public ClientForm()
+        {
+            InitializeComponent();
+
+            SetIPAddress();
+        }
+
+        private void SetIPAddress()
+        {
+            string hostName = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(hostName);
+            txtIP.Text = ipHost.AddressList[ipHost.AddressList.Length - 1].ToString();
+        }
+
+
+        private void UpdateGUI(bool IsConnected)
+        {
+            btnConnect.Enabled = !IsConnected;
+            btnSend.Enabled = IsConnected;
+            lblStatus.Text = IsConnected ? "Connected" : "Disconnected";
+            lblStatus.ForeColor = IsConnected ? Color.Green : Color.Red;
+        }
+
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            // Connect to server
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            int portNum = int.Parse(txtPort.Text);
+            IPAddress ip = IPAddress.Parse(txtIP.Text);
+            client.Connect(new IPEndPoint(ip, portNum));
+
+            UpdateGUI(true);
+
+            WriteToLog("Connected to Server..." + Environment.NewLine);
+
+            WaitingForData(client);
+        }
+
+
+        private void WaitingForData(Socket client)
+        {
+            client.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(OnReceive), null);
+        }
+
+
+        private void OnReceive(IAsyncResult ar)
+        {
+            client.EndReceive(ar);
+
+            string msg = Encoding.Unicode.GetString(buffer);
+
+            WriteToLog(msg);
+
+            WaitingForData(client);
+        }
+
+        private void WriteToLog(string msg)
+        {
+            MethodInvoker invoker = new MethodInvoker(delegate { txtLog.AppendText("Server said: " + msg + Environment.NewLine); });
+            this.BeginInvoke(invoker);
+        }
+
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string msg = txtMsg.Text;
+            buffer = Encoding.Unicode.GetBytes(msg);
+
+            client.Send(buffer);
+        }
+    }
+}
